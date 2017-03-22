329601223fdd9bd4dda8f7f47586de0c274fa70a
Ivan Vercinsky <ivan.vercinsky@gdsmodellica.com>
2017-03-22 14:48:04 -0300

    refactor

--- parser.js
+++ parser.js
@@ -4,11 +4,13 @@ var parse = require('xml-parser');
 var xml2js = require('xml2js');
 var async = require('async');
 
-var dirPath = "arfs/"; //provice here your path to dir
+var dirPath = process.env.ARFS_PATH + "/"; //provice here your path to dir
+var outputPath = process.env.OUTPUT_PATH;
 var globalFilesPath = [];
 parseArfs(dirPath);
 
 function parseArfs(dirPath) {
+    console.log("Parsing...");
     fs.readdir(dirPath, readDirectory);
 }
 
@@ -48,23 +50,23 @@ function ensureDirectoryExistence(filePath) {
 }
 
 function writeArfs(arfsMap) {
-    fs.removeSync(__dirname+'/output');       
-    ensureDirectoryExistence(__dirname+"/output/a");            
+    fs.removeSync(__dirname+'/'+outputPath);       
+    ensureDirectoryExistence(__dirname+"/"+outputPath+"/a");            
     arfsMap.forEach((arfs, key) => {
         arfs.map((arf) => {
             var fileName = arf.fileName.split("/")[1].split(".")[0]+".xml";
-            var savePath = __dirname+"/output/"+key+"/"+fileName;
+            var savePath = __dirname+"/"+outputPath+"/"+key+"/"+fileName;
             ensureDirectoryExistence(savePath);
             fs.readFile(arf.fileName, 'utf8', function(err, result) {
                 fs.writeFile(savePath, result, function(err) {
                     if(err) {
                         return console.log(err);
-                    }
-                    console.log("Writing arf", arf.data.DatApellido, "in folder" , key);            
+                    }            
                 });
             });            
-        })
+        });
     });
+    console.log("Done!");
 }
 
 function groupByRecordId(array) {
