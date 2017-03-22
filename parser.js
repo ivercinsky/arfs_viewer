d45e3423e2850b1a0caf654839bdfed0c6c94592
Ivan Vercinsky <ivan.vercinsky@gdsmodellica.com>
2017-03-22 11:27:55 -0300

    refactor

--- parser.js
+++ parser.js
@@ -5,37 +5,38 @@ var xml2js = require('xml2js');
 var async = require('async');
 
 var dirPath = "arfs/"; //provice here your path to dir
-console.log(dirPath);
-fs.readdir(dirPath, function (err, filesPath) {
+var globalFilesPath = [];
+parseArfs(dirPath);
+
+function parseArfs(dirPath) {
+    fs.readdir(dirPath, readDirectory);
+}
+
+function readDirectory(err, filesPath) {
     if (err) throw err;
-    filesPath = filesPath.map(function(filePath){ //generating paths to file
-        return dirPath + filePath;
-    });
-    async.map(filesPath, function(filePath, cb){ //reading files or dir
-        fs.readFile(filePath, 'utf8', cb);
-    }, function(err, results) {
-        //console.log(filesPath);
- 
-        var parsed = results.map((arf) => {
+    globalFilesPath = getFilePaths(dirPath, filesPath);
+    async.map(globalFilesPath, readFile, processFiles);
+}
+
+function readFile(filePath, cb) {
+    fs.readFile(filePath, 'utf8', cb);
+}
+function processFiles(err, filesData) {
+        var parsed = filesData.map((arf) => {
             return parse(arf);
         })
-
-        var joined = filesPath.map((file, index) => {
+        var joined = globalFilesPath.map((file, index) => {
             return {fileName: file, data: extractData(parsed[index])};
         })
-        
-        //console.log(joined[0].data.CodigoDeOperacion);
 
         var grouped = groupByRecordId(joined);
-        
-        //console.log(grouped);
-
         writeArfs(grouped);
-        //console.log(parsed); //this is state when all files are completely read
-        
-    });
-});
-
+}
+function getFilePaths(dirPath, filesPath) {
+    return filesPath.map(function(filePath){ //generating paths to file
+        return dirPath + filePath;
+    });   
+}
 function ensureDirectoryExistence(filePath) {
     var dirname = path.dirname(filePath);
     //console.log(dirname);
