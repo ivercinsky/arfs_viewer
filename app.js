03f681028bb719e5b856d93011390adc5b6813cd
Ivan Vercinsky <ivan.vercinsky@gdsmodellica.com>
2017-04-26 17:23:19 -0300

    upload arfs zip and redirect

--- app.js
+++ app.js
@@ -10,8 +10,11 @@ var users = require('./routes/users');
 var arfs = require('./routes/arfs');
 var arf = require('./routes/arf');
 var parser = require('./parser.js');
-
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/'});
+var fs = require('fs');
 var app = express();
+const decompress = require('decompress');
 parser.parse();
 
 // view engine setup
@@ -22,7 +25,7 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/output', serveIndex(__dirname+'/public/output'));
@@ -31,6 +34,12 @@ app.use('/', index);
 app.use('/users', users);
 app.use('/arfs', arfs);
 app.use('/arfs/:dir', arf);
+app.post("/arfs_file", upload.single('arfs_file'), function(req, res, next){
+  decompress(path.join(__dirname, `uploads/${req.file.filename}`), path.join(__dirname, 'arfs')).then(files => {
+    res.sendStatus(200).send("OK");
+  });
+  
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router({ mergeParams : true });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Arfs Viewer' });
 });
 
 module.exports = router;
